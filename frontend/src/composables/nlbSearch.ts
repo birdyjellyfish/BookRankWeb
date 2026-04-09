2b82c38fbade3e001b796ba429abb81afc9ede6b
birdyjellyfish <moses.chiacy@gmail.com>
2026-04-09 18:31:16 +0800

    added refs to useNlbSearch composable

--- frontend/src/composables/nlbSearch.ts
+++ frontend/src/composables/nlbSearch.ts
@@ -1,3 +1,11 @@
-export function useNlbSearch() {
+import { ref } from "vue";
+
+export function useNlbSearch(isbn: string, title: string, authors: string) {
+  // 2-stage book search
+  // first directly check availbility via isbn
+  // if not fall back to title search
+  const loading = ref(false);
+  const availbility = ref<string[]>([]);
+  const error = ref("");
   
 }
