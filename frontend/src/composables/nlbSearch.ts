2fe1ec48517a56d365c0cfc6e46f787e2d86d683
birdyjellyfish <moses.chiacy@gmail.com>
2026-04-10 00:23:27 +0800

    added .env.example and updated nlbSearch code

--- frontend/src/composables/nlbSearch.ts
+++ frontend/src/composables/nlbSearch.ts
@@ -1,11 +1,44 @@
 import { ref } from "vue";
 
-export function useNlbSearch(isbn: string, title: string, authors: string) {
+const availibilityUrl = "https://openweb.nlb.gov.sg/api/v2/Catalogue/GetAvailabilityInfo"
+
+export function useNlbSearch() {
   // 2-stage book search
-  // first directly check availbility via isbn
+  // first directly check availibility via isbn
   // if not fall back to title search
   const loading = ref(false);
   const availbility = ref<string[]>([]);
   const error = ref("");
+
+  async function getBookAvailibiity(isbn: string, authors: string, title: string) {
+    loading.value = true;
+    availbility.value = [];
+    error.value = "";
+
+    //left pad isbn with 0 to make it isbn10
+    isbn = isbn.padStart(10, '0');
+
+    // first directly check availibility via isbn
+    try {
+      const response = await fetch(`${availibilityUrl}?ISBN=${isbn}`, {
+        method: "GET",
+        headers: {
+          "X-Api-Key":
+        },
+        credentials: 'include'
+      })
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      } else {
+        success.value = "Successfully retrieved book records.";
+        data.value = await response.json();
+      }
+    } catch (err) {
+      error.value = (err as Error).message;
+    } finally {
+      loading.value = false;
+    }
+  } 
   
 }
